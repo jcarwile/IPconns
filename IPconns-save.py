1c08f0c291931819b6485d03ea7152ed6b1b1ca8
jcarwile <jcarsgm@gmail.com>
2017-04-13 16:19:24 -0400

    xxx

--- IPconns-save.py
+++ IPconns-save.py
@@ -75,70 +75,6 @@ def total_rem_conns_by_user(e_array):
 
     return tot_array
 
-def count_tn3270_subnets(e_array):
-    subnet_array = []
-    for item in e_array:
-        if item[0] == 'TN3270':
-            rm_ip = str(item[3])
-            oct1, oct2, oct3, oct4 = rm_ip.split('.')
-            subnet = oct1 + '.' + oct2 + '.' + oct3
-            if subnet not in subnet_array:
-                subnet_array.append(subnet)
-    return subnet_array
-
-def print_listeners(l_conns):
-    tmp_cnt = 0
-    print 'This is the list of Listening ports'
-    for item in l_conns:
-        user = item[0]
-        loc_port = item[2]
-        print user, loc_port
-        tmp_cnt += 1
-    print 'Total Listening ports', tmp_cnt
-    return tmp_cnt
-
-
-def print_internals(i_conns):
-    tmp_cnt = 0
-    print '\n\nThis is the list of Internal connections'
-    for item in i_conns:
-        user = item[0]
-        loc_port = item[2]
-        state = item[5]
-        print user, loc_port, state
-        tmp_cnt += 1
-    print 'Total Internal connections', tmp_cnt
-    return tmp_cnt
-
-
-def print_others(o_conns):
-    tmp_cnt = 0
-    print '\n\nThis is the list of Other connection states'
-    for item in o_conns:
-        user = item[0]
-        loc_port = item[2]
-        rem_ip = item[3]
-        rem_port = item[4]
-        state = item[5]
-        print user, loc_port, rem_ip, rem_port, state
-        tmp_cnt += 1
-    print 'Total Other connections'
-    return tmp_cnt
-
-
-def print_established(e_conns):
-    t_array = total_rem_conns_by_user(e_conns)
-    print '\n\nThis is the list of Established connections with count greater than 1'
-    print '{0:8}   {1:5}   {2:15}   {3:5}'.format('Name', 'Port', 'Remote IP', 'Count')
-    print '{0:8}   {1:5}   {2:15}   {3:5}'.format('------', '----', '---------------', '---')
-    calc_tn3270_cnt = 0
-    for item in t_array:
-        calc_tn3270_cnt += item[1]
-        if item[1] > 1:
-            user, port, ip = item[0].split()
-            print '{0:8}   {1:5}   {2:18}   {3:5d}'.format(user, port, ip, item[1])
-    return calc_tn3270_cnt
-
 
 if __name__ == '__main__':
     conn_lines = get_conns_array()
@@ -165,22 +101,52 @@ if __name__ == '__main__':
 
     tot_cnt = 0
 
-    l_cnt = print_listeners(Listen_conns)
-    tot_cnt += l_cnt
+    tmp_cnt = 0
+    print 'This is the list of Listening ports'
+    for item in Listen_conns:
+        user = item[0]
+        loc_port = item[2]
+        print user, loc_port
+        tmp_cnt += 1
+    print 'Total Listening ports', tmp_cnt
+    tot_cnt += tmp_cnt
 
-    i_cnt = print_internals(Internal_conns)
-    tot_cnt += i_cnt
+    tmp_cnt = 0
+    print '\n\nThis is the list of Internal connections'
+    for item in Internal_conns:
+        user = item[0]
+        loc_port = item[2]
+        state = item[5]
+        print user, loc_port, state
+        tmp_cnt += 1
+    print 'Total Internal connections', tmp_cnt
+    tot_cnt += tmp_cnt
 
-    o_cnt = print_others(Other_conns)
-    tot_cnt += o_cnt
+    tmp_cnt = 0
+    print '\n\nThis is the list of Other connection states'
+    for item in Other_conns:
+        user = item[0]
+        loc_port = item[2]
+        rem_ip = item[3]
+        rem_port = item[4]
+        state = item[5]
+        print user, loc_port, rem_ip, rem_port, state
+        tmp_cnt += 1
+    print 'Total Other connections'
+    tot_cnt += tmp_cnt
 
     print '\n\nTotal of all non-Established connections', tot_cnt
 
-    e_cnt = print_established(Establish_conns)
-    print '\n\nThe total number of calculated Established connections:', e_cnt
+    t_array = total_rem_conns_by_user(Establish_conns)
+    print '\n\nThis is the list of Established connections with count greater than 1'
+    print '{0:8}   {1:5}   {2:15}   {3:5}'.format('Name', 'Port', 'Remote IP', 'Count')
+    print '{0:8}   {1:5}   {2:15}   {3:5}'.format('------', '----', '---------------', '---')
+    calc_tn3270_cnt = 0
+    for item in t_array:
+        calc_tn3270_cnt += item[1]
+        if item[1] > 1:
+            user, port, ip = item[0].split()
+            print '{0:8}   {1:5}   {2:18}   {3:5d}'.format(user, port, ip, item[1])
 
-    sn_array = count_tn3270_subnets(Establish_conns)
-    print 'Total of different TN3270 subnets:', len(sn_array)
-    sn_array.sort()
-    for sub in sn_array:
-        print sub
+    print 'The total number of calculated Established connections:', calc_tn3270_cnt
+
